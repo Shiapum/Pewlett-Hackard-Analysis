652420bddd755c2d2408d06371fd49104cf90954
Shiapum <h.reyes.1292@gmail.com>
2021-11-07 21:32:24 -0600

    Challenge uploaded

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+-- The Number of Retiring Employees by Title
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- retiring_titles
+SELECT COUNT(emp_no) AS number_employees, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY number_employees DESC;
+
+-- mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
